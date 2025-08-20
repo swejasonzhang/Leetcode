6800c7a5886f63bc8299b7a383a4d91fcc24f48c
swejasonzhang <swejasonzhang@gmail.com>
2025-08-19 23:39:19 -0400

    Finished Question 1876

--- Easy.py
+++ Easy.py
@@ -1,5 +1,4 @@
 # 2148. Count Elements With Strictly Smaller and Greater Elements 
-
 # Given an integer array nums, return the number of elements that have both a strictly smaller and a strictly greater element appear in nums.
 
 # Example 1:
@@ -9,13 +8,13 @@
 # Explanation: The element 7 has the element 2 strictly smaller than it and the element 11 strictly greater than it.
 # Element 11 has element 7 strictly smaller than it and element 15 strictly greater than it.
 # In total there are 2 elements having both a strictly smaller and a strictly greater element appear in nums.
+
 # Example 2:
 
 # Input: nums = [-3,3,3,90]
 # Output: 2
 # Explanation: The element 3 has the element -3 strictly smaller than it and the element 90 strictly greater than it.
 # Since there are two elements with the value 3, in total there are 2 elements having both a strictly smaller and a strictly greater element appear in nums.
- 
 
 # Constraints:
 
@@ -37,11 +36,8 @@ class Solution(object):
 # Space Complexity - O(N) - Result up to the amount of elements in nums. Remove is just 2 elements regardless of element size. O(N) + O(1) = O(N). N still being the number of elements in the list.
 
 # 1221. Split a String in Balanced Strings 
-
 # Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
-
 # Given a balanced string s, split it into some number of substrings such that:
-
 # Each substring is balanced.
 # Return the maximum number of balanced strings you can obtain.
 
@@ -50,6 +46,7 @@ class Solution(object):
 # Input: s = "RLRRLLRLRL"
 # Output: 4
 # Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
 # Example 2:
 
 # Input: s = "RLRRRLLRLL"
@@ -62,7 +59,6 @@ class Solution(object):
 # Output: 1
 # Explanation: s can be split into "LLLLRRRR".
  
-
 # Constraints:
 
 # 2 <= s.length <= 1000
@@ -98,7 +94,6 @@ class Solution(object):
 # 766. Toeplitz Matrix
 
 # Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
-
 # A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
 
 # Example 1:
@@ -109,15 +104,14 @@ class Solution(object):
 # In the above grid, the diagonals are:
 # "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
 # In each diagonal all elements are the same, so the answer is True.
-# Example 2:
 
+# Example 2:
 
 # Input: matrix = [[1,2],[2,2]]
 # Output: false
 # Explanation:
 # The diagonal "[1, 2]" has different elements.
  
-
 # Constraints:
 
 # m == matrix.length
@@ -144,3 +138,53 @@ class Solution(object):
     
 # Time Complexity - O(M * N) - The first loop iterates through all of the elements which is O(M) and the second is nested within it which is O(N) which makes it O(M * N). M being the amount of rows and N being the amount of columns since not every matrix is equal.
 # Space Complexity - O(1) - The variables don't factor the matrix into consideration so space will always be constant regardless of input size.
+
+# 1876. Substrings of Size Three with Distinct Characters
+
+# A string is good if there are no repeated characters.
+# Given a string s​​​​​, return the number of good substrings of length three in s​​​​​​.
+# Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+# A substring is a contiguous sequence of characters in a string.
+
+# Example 1:
+
+# Input: s = "xyzzaz"
+# Output: 1
+# Explanation: There are 4 substrings of size 3: "xyz", "yzz", "zza", and "zaz". 
+# The only good substring of length 3 is "xyz".
+# Example 2:
+
+# Input: s = "aababcabc"
+# Output: 4
+# Explanation: There are 7 substrings of size 3: "aab", "aba", "bab", "abc", "bca", "cab", and "abc".
+# The good substrings are "abc", "bca", "cab", and "abc".
+
+# Constraints:
+
+# 1 <= s.length <= 100
+# s​​​​​​ consists of lowercase English letters.
+
+class Solution(object):
+    def countGoodSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        counter = 0
+        i , j = 0, 2
+
+        while j < len(s):
+            substring = s[i: j + 1] # Substring of length 3 
+
+            if (len(set(substring)) == 3): # If there are no dups and the length is 3 then it's a valid substring
+                counter += 1
+
+            i += 1
+            j += 1
+
+        return counter
+    
+# Time Complexity - O(N) - The while loop iterates through every element. N is defined as the amount of chars in the string.
+# Space Complexity - O(1) - The variables don't factor the string into consideration so space will always be constant regardless of input size.
+
