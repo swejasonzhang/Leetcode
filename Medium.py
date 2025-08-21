e81f054dbe1fb2feee2aebd435494c30f7c36c93
swejasonzhang <swejasonzhang@gmail.com>
2025-08-21 18:52:59 -0400

    Finished Question 1695

--- Medium.py
+++ Medium.py
@@ -182,3 +182,55 @@ class Solution(object):
 # Time Complexity - O(N) - The while loop iterates through the whole string once to check for valid partitions. In this case N refers to the length of the string. 
 # Space Complexity - O(1) - I am not taking up any space relative to the chars within the string. All of my variables are independant and don't consider string size.
 
+# 1695. Maximum Erasure Value
+
+# You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is equal to the sum of its elements.
+# Return the maximum score you can get by erasing exactly one subarray.
+# An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+# Approach: Use sliding window. As long as there are no dups then keep increasing the set size then compare the current sum to the result. Once J hits the last index return the largest sum.
+
+# Example 1:
+
+# Input: nums = [4,2,4,5,6]
+# Output: 17
+# Explanation: The optimal subarray here is [2,4,5,6].
+
+# Example 2:
+
+# Input: nums = [5,2,1,2,5,2,1,2,5]
+# Output: 8
+# Explanation: The optimal subarray here is [5,2,1] or [1,2,5].
+ 
+# Constraints:
+
+# 1 <= nums.length <= 105
+# 1 <= nums[i] <= 104
+
+class Solution(object):
+    def maximumUniqueSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        result = 0 
+        i, j = 0, 0 
+        elementSet = set() 
+        currentSum = 0  
+
+        while j < len(nums): 
+            if nums[j] not in elementSet:
+                elementSet.add(nums[j]) # add the element into the set
+                currentSum += nums[j] # sum it up
+                result = max(result, currentSum) # compare 
+                j += 1
+            else:
+                elementSet.remove(nums[i]) # remove the dup from the start of the set until it's gone
+                currentSum -= nums[i] # remove from the current sum
+                i += 1
+
+        return result
+    
+# Time Complexity - O(N) - The while loop iterates through the whole string once to check for dups. In this case N refers to the length of the array
+# Space Complexity - O(N) - I am creating a set thus I am managing elements relative to the nums array. N refers to every element within the array
