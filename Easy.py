5b19e21be8c684722342ac2e6e38a5a81c2952bd
swejasonzhang <swejasonzhang@gmail.com>
2025-08-30 00:01:09 -0400

    Added name

--- Easy.py
+++ Easy.py
@@ -312,6 +312,8 @@ class Solution(object):
 # Time Complexity: O(N) — We check each rectangle once, and all operations per rectangle take constant time.
 # Space Complexity: O(1) — We only use a fixed number of variables regardless of the number of rectangles.
 
+# 231. Power of Two
+
 # Given an integer n, return true if it is a power of two. Otherwise, return false.
 # An integer n is a power of two, if there exists an integer x such that n == 2x.
 
